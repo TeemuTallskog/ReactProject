5c07d702a0f2c0d2c6339f1b9353cc00fbeba1af
TeemuTallskog <72005659+TeemuTallskog@users.noreply.github.com>
2022-10-03 17:51:24 +0300

    post styling

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,16 +8,17 @@ import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import {useNavigate, createSearchParams, Link} from "react-router-dom";
 import profileImg from '../resources/images/profile_img_default.png'
 import axios from "axios";
+import '../resources/css/post.css';
 
-function Post(post){
+function Post(post) {
     const navigate = useNavigate();
     const [likeStatus, setLikeStatus] = useState(post.post.user_like_status);
     const [totalLikes, setTotalLikes] = useState(post.post.total_likes);
     const [likeIcon, setLikeIcon] = useState(false);
     const [replyContent, setReplyContent] = useState(null);
 
-    const likePost = function (){
-        (async() =>{
+    const likePost = function () {
+        (async () => {
             const response = await fetch('http://localhost:8080/like', {
                 method: 'POST',
                 headers: {
@@ -27,73 +28,78 @@ function Post(post){
                 },
                 body: JSON.stringify({post_id: post.post.post_id})
             });
-            if(response.status === 202){
-                response.json().then((data)=>{
+            if (response.status === 202) {
+                response.json().then((data) => {
                     console.log(data);
                     setTotalLikes(data.total_likes);
                     setLikeStatus(data.user_like_status);
                 })
             }
 
-        })().catch((e)=> console.log(e));
+        })().catch((e) => console.log(e));
     }
 
-    const getReplyContent = async function(){
-        if(post.post.reply_to){
-            const response = await axios.get('http://localhost:8080/post',{
-                params:{post_id: post.post.reply_to},
-                headers: {'Authorization' : localStorage.getItem("accessToken")}
+    const getReplyContent = async function () {
+        if (post.post.reply_to) {
+            const response = await axios.get('http://localhost:8080/post', {
+                params: {post_id: post.post.reply_to},
+                headers: {'Authorization': localStorage.getItem("accessToken")}
             }).catch((e) => {
                 console.log(e);
                 setReplyContent(<p>error</p>);
             });
-            let returnString =response.data.post[0].content;
-            if(returnString.length > 18){
-                returnString = returnString.substring(0,18) + "...";
+            let returnString = response.data.post[0].content;
+            if (returnString.length > 18) {
+                returnString = returnString.substring(0, 18) + "...";
             }
             setReplyContent(<div><h6>{response.data.post[0].username}:</h6><p>{returnString}</p></div>);
         }
     }
 
-    useEffect(()=>{
-        if(likeStatus == 1){
+    useEffect(() => {
+        if (likeStatus == 1) {
             setLikeIcon(<FavoriteIcon/>);
-        }
-        if(post.post.reply_to){
-            getReplyContent();
-        }
-        else{
+        }else {
             setLikeIcon(<FavoriteBorderIcon/>);
+        }
+        if (post.post.reply_to) {
+            getReplyContent();
         }
     }, [likeStatus, post.post.reply_to])
 
-    return(
+    return (
         <Card style={{margin: '10px'}}>
             <Card.Body>
-                {post.post.reply_to && <Card.Header><Link to={{pathname:'/Post', search:'?post_id=' + post.post.reply_to}}>{replyContent}</Link> </Card.Header>}
-                <img style={{borderRadius: '50%', width: '48px'}}
-                     src={post.post.profile_img? post.post.profile_img : profileImg}
-                     onError={({currentTarget}) => {
-                         currentTarget.onerror = null;
-                         currentTarget.src = profileImg;
-                     }}/>
-                <Card.Title>{post.post.username}</Card.Title>
-                <Card.Text>
-                    {post.post.content}
-                </Card.Text>
-                <Row style={{width: '50%', margin: 'auto', padding: '0'}}>
-                    <Col>
-                        <button className='button' onClick={() => navigate({
-                            pathname: '/Post',
-                            search: `?${createSearchParams({post_id: post.post.post_id})}`
-                        })}><ChatBubbleOutlineIcon/></button>
-                        <p>{post.post.reply_count}</p>
-                    </Col>
-                    <Col>
-                        <button onClick={likePost} className='button'>{likeIcon}</button>
-                        <p>{totalLikes}</p>
-                    </Col>
-                </Row>
+                {post.post.reply_to && <Card.Header><Link
+                    to={{pathname: '/Post', search: '?post_id=' + post.post.reply_to}}>{replyContent}</Link>
+                </Card.Header>}
+                <div className="post-body">
+                    <img  className="post-profile-picture" style={{borderRadius: '50%', width: '48px'}}
+                         src={post.post.profile_img ? post.post.profile_img : profileImg}
+                         onError={({currentTarget}) => {
+                             currentTarget.onerror = null;
+                             currentTarget.src = profileImg;
+                         }}/>
+                    <div className="post-body-content">
+                        <Card.Title>{post.post.username}</Card.Title>
+                        <Card.Text>
+                            {post.post.content}
+                        </Card.Text>
+                        <div className="post-interaction-buttons">
+                            <div className="reply-icon-button-container">
+                                <button className='button' onClick={() => navigate({
+                                    pathname: '/Post',
+                                    search: `?${createSearchParams({post_id: post.post.post_id})}`
+                                })}><ChatBubbleOutlineIcon/></button>
+                                <p>{post.post.reply_count}</p>
+                            </div>
+                            <div className="like-icon-button-container">
+                                <button onClick={likePost} className='button'>{likeIcon}</button>
+                                <p>{totalLikes}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Card.Footer>{moment(post.post.created).fromNow()}</Card.Footer>
             </Card.Body>
         </Card>
